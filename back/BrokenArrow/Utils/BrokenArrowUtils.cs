019bbf5cb79d2aa778e0475638ac238677c217da
s4v4te__974 <stephane.kouotze@gmail.com>
2024-06-09 13:26:00 -0400

    Update utils

--- back/BrokenArrow/Utils/BrokenArrowUtils.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BrokenArrow.Utils
-{
-    public static class BrokenArrowUtils
-    {
-
-        public const string UNABLE_TO_RETRIEVE_BA = "Unable to retrieve all Broken Arrows";
-    }
-}
