c3996268575ff84e44f795a0723726b727fa4504
s4v4te__974 <stephane.kouotze@gmail.com>
2024-06-16 00:51:50 -0400

    update entites

--- back/BrokenArrowApp/Models/DbSet/Description.cs
+++ back/BrokenArrowApp/Models/DbSet/Description.cs
@@ -1,12 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BrokenArrowApp.Models.Entities
 {
+    [Table("description")]
     public class Description
     {
         [Key]
+        [Column("fulldescriptionid")]
         public Guid FullDescriptionId { get; set; }
 
+        [Column("fulldescription")]
         public string? FullDescription { get; set; }
 
         public BrokenArrow? BrokenArrow { get; set; }
--- back/BrokenArrowApp/Models/DbSet/Vehicule.cs
+++ back/BrokenArrowApp/Models/DbSet/Vehicule.cs
@@ -1,19 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BrokenArrowApp.Models.Entities
 {
+    [Table("vehicule")]
     public class Vehicule
     {
 
         [Key]
+        [Column("vehiculeid")]
         public Guid VehiculeId { get; set; }
 
+        [Column("type")]
         public string? Type { get; set; }
 
+        [Column("builder")]
         public string? Builder { get; set; }
 
+        [Column("name")]
         public string? Name { set; get; }
 
+        [Column("vehiculedescription")]
         public string? VehiculeDescription { set; get; }
 
         public List<BrokenArrow>? BrokenArrows { get; set; }
