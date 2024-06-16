57ab5fc75456db324e51bacaa0ae46b13752178b
s4v4te__974 <stephane.kouotze@gmail.com>
2024-06-16 00:53:42 -0400

    Update Program.cs

--- back/BrokenArrowApp/Program.cs
+++ back/BrokenArrowApp/Program.cs
@@ -1,7 +1,19 @@
+using BrokenArrowApp.Controllers.Handler;
 using BrokenArrowApp.Data;
+using BrokenArrowApp.Service;
+using BrokenArrowApp.Service.Impl;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("localSwagger",
+                      builder =>
+                      {
+                          builder.WithOrigins("http://localhost:5280/").AllowAnyMethod().AllowAnyHeader();
+                      });
+});
 
 // Add services to the container.
 
@@ -11,12 +23,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+// add services
+builder.Services.AddScoped<AutoMapper.Mapper>();
+builder.Services.AddScoped<IVehiculeService, VehiculeServiceImpl>();
+
 
 // database connexion build the context
 builder.Services.AddDbContext<BrokenArrowContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("MyDatabase")));
 
 var app = builder.Build();
+app.UseMiddleware<ErrorHandler>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,6 +41,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("localSwagger");
 
 app.UseAuthorization();
 
