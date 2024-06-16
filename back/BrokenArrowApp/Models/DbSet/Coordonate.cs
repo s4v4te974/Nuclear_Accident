d360177cbd6a2547f53d4a63227cacb9999fdddf
s4v4te__974 <stephane.kouotze@gmail.com>
2024-06-16 00:50:59 -0400

    update entities

--- back/BrokenArrowApp/Models/DbSet/Coordonate.cs
+++ back/BrokenArrowApp/Models/DbSet/Coordonate.cs
@@ -1,19 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BrokenArrowApp.Models.Entities
 {
+    [Table("coordonate")]
     public class Coordonate
     {
 
         [Key]
+        [Column("coordonateid")]
         public Guid CoordonateId { get; set; }
 
+        [Column("countryname")]
         public string? CountryName { get; set; }
 
+        [Column("positionlost")]
         public string? PositionLost { get; set; }
 
+        [Column("xcoordonate")]
         public float? XCoordonate { get; set; }
 
+        [Column("ycoordonate")]
         public float? YCoordonate { get; set; }
 
         public BrokenArrow? BrokenArrow { get; set; }
